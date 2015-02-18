f8831084ed07be95b79045bcc6c3e1575aa01c9c
Christine McClure <camcclure@gmail.com>
2015-02-18 12:48:25 -0600

    skeletal toc page

---
+++ web/toc.php
@@ -0,0 +1,35 @@
+<?php include("/includes/head.html") ?>
+<body>
+    <?php include("/includes/nav.html") ?>
+  
+	<div class="row">
+		<div class="large-8 columns">
+      <h1>Table of Contents</h1>
+      <ul class="no-dec">
+        <li>Figures</li>
+        <li>Tables</li>
+        <li>Executive Summary</li>
+        <li>Implementing a Content Strategy</li>
+        <li class="sub1">Content Inventory</li>
+        <li class="sub1">Content Layout</li>
+        <li class="sub2">Microinteractions</li>
+        <li class="sub2">Increase Readability</li>
+        <li>Highlighting electronic resources</li>
+        <li class="sub1">Remove the catalog search from the home page</li>
+        <li class="sub1">Add a database quick search on the new home page</li>
+        <li>User-Centered Design</li>
+        <li class="sub1">Customize the main navigation to library’s needs</li>
+        <li class="sub2">Custom sort for database subjects</li>
+        <li class="sub1">User personas</li>
+        <li class="sub1">User experience testing</li>
+        <li>Conclusion</li>
+        <li>Bibliography</li>
+        <li>Appendix A: User Personas</li>
+        <li>Javier</li>
+        <li>Rebecca</li>
+      </ul>
+
+    </div>  
+	</div>
+
+<?php include("/includes/footer.html") ?>
