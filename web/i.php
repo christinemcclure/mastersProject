aa2ac8ea9ce7527d3f9f472871054b8232fca02a
Christine McClure <camcclure@gmail.com>
2015-02-18 13:33:22 -0600

    added figures and table page stumps

---
+++ web/i.php
@@ -0,0 +1,15 @@
+<?php include("/includes/head.html") ?>
+<body>
+    <?php include("/includes/nav.html") ?>
+  
+	<div class="row">
+		<div class="large-8 columns">
+      <h1>Table of Figures</h1>
+      <ul class="no-dec">
+        <li></li>
+      </ul>
+
+    </div>  
+	</div>
+
+<?php include("/includes/footer.html") ?>
