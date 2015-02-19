e1a418f760d3c83741bfd9b46812473a4dc3554d
Christine McClure <camcclure@gmail.com>
2015-02-19 15:06:53 -0600

    trying sticky

--- web/index.php
+++ web/index.php
@@ -2,8 +2,7 @@
 <body>
   <?php include("/includes/nav1.html") ?>
 
-  <section class="main-section">
-  
+  <section>
 	<div class="row">
 		<div class="large-12 columns">
 			<h1>Galvin Library Website Redesign</h1>
@@ -14,14 +13,17 @@
       <h3>Spring 2015</h3>
     </div>  
 	</div>
-
+  </section> 
+  
+  <section>
 	<div class="row" id="figures">
 		<div class="large-8 columns">
 			<h1>Table of Contents</h1>
       <?php include("toc.html") ?>    
     </div>  
 	</div>
-    
+</section>
+  
 	<div class="row" id="figures">
 		<div class="large-8 columns">
 			<h1>Table of Figures</h1>
@@ -35,8 +37,14 @@
       <?php include("tables.html") ?>    
     </div>  
 	</div>     
-  </section>   
-
-    <?php include("/includes/nav2-and-footer.html") ?>
+    
+	<div class="row">
+		<div class="large-12 columns">
+			<h1>Executive Summary</h1>
+      <?php include("summary.html") ?>    
+    </div>  
+	</div>    
+    
   
-<?php include("/includes/footer.html") ?>
+<?php include("/includes/nav2-and-footer.html") ?>
+  
