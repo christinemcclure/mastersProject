a0a514951c97dfb256f15cd87d85d03f6f5851a3
Christine McClure <camcclure@gmail.com>
2015-02-21 08:25:07 -0600

    toc navigation and links complete

--- web/index.php
+++ web/index.php
@@ -112,9 +112,9 @@
 	  <div class="row">
       <div class="large-12 columns">
         <h1>Appendix B: User Personas</h1>
-        <h2>Javier</h2>
+        <h2 class="chapter" id="javier">Javier</h2>
         <img src="assets/images/javier.jpg" />   
-        <h2>Rebecca</h2>
+        <h2 class="chapter" id="rebecca">Rebecca</h2>
         <img src="assets/images/rebecca.jpg" />
       </div>
   </div>
